0888b4f8e7099c0849a894bf07b80f89ae8db4d1
Umangi Nigam <125351370+Umanginigam@users.noreply.github.com>
2025-12-18 16:11:06 +0530

    Update app.py

--- backend/api/app.py
+++ backend/api/app.py
@@ -26,6 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://gen-ai-assessment-recommendation-sy-tau.vercel.app"
        
     ],
     allow_credentials=True,
