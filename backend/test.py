b9798b645d5a470b07dbe19fc51117a51b630f3b
Umangi <mac@Macs-MacBook-Air.local>
2025-12-18 11:12:57 +0530

    Merge

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-from backend.pipeline import recommend
-
-results = recommend(
-    "Java developer who works with business stakeholders",
-    max_results=10
-)
-
-for r in results:
-    print(r["assessment_name"], r["test_type"])
